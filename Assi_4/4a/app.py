70c384b7e24ddf52e41064d3a89ff32b8bbd812d
thebabellibrarybot <jack@thebabellibrary.org>
2023-01-25 07:51:50 -0500

    otw to work

--- Assi_4/4a/app.py
+++ Assi_4/4a/app.py
@@ -33,6 +33,8 @@ def read_csv(tsv_path, delimiter = ','):
 #i = read_csv(tsv_path)
 #print(i)
 
+#TODO all the below functions need to reference 'status_updates' not 'tsv_path'... aka read lists not files.tsv
+
 #1b
 def get_update_most_responded_to(tsv_path, kwarg):
     """
@@ -69,16 +71,38 @@ def get_logest_update(status_updates, length_type = "tokens"):
     df = pd.read_csv(tsv_path, sep='\t')
     longest = 0
     longest_update = ''
+
     for i in df.index:
+
         if length_type == 'tokens':
-            longest_tok = tok.word_tokenize(df['status_message'][i])
-            if len(longest_update) > longest:
+            longest_tok = tok.word_tokenize(str(df['status_message'][i]))
+            if len(longest_tok) > longest:
+                longest = len(longest_tok)
                 longest_update = df['status_message'][i]
 
         elif length_type == 'sentences':
-            i = 'i'
+            longest_sent = tok.sent_tokenize(str(df['status_message'][i]))
+            if len(longest_sent) > longest:
+                longest = len(longest_sent)
+                longest_update = df['status_message'][i]
+
         elif length_type == 'characters':
-            i = 'i'
-    return longest_update
-i = get_logest_update(tsv_path, 'tokens')
-print(i)
+            longest_chars = (str(df['status_message'][i]))
+            if len(longest_chars) > longest:
+                longest = len(longest_chars)
+                longest_update = df['status_message'][i]    
+            
+    return (longest_update)
+
+
+#i = get_logest_update(tsv_path, 'tokens')
+#print(i)
+
+#1d
+def get_updates_with_keywords(tsv_path, keywords, case_sensitive = False):
+
+    
+    filtered_status_updates = ''
+
+    return filtered_status_updates
+# i = get_updates_with_keywords(tsv_path, keywords, case_sensitive)
