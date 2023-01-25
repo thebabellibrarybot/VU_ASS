a4e47701c1faa573deda546782b9fa286c43197c
thebabellibrarybot <jack@thebabel.org>
2023-01-25 10:43:22 -0500

    new book

--- Assi_4/4a/app.py
+++ Assi_4/4a/app.py
@@ -10,31 +10,30 @@ def read_csv(tsv_path, delimiter = ','):
 
     csv_dic_ls = []
     for i in df.index:
-        if i < 5:
-            csv_dic = {}
-            csv_dic['link_name'] = df['link_name'][i]
-            csv_dic['num_angrys'] = df['num_angrys'][i]
-            csv_dic['num_comments'] = df['num_comments'][i]
-            csv_dic['num_hahas'] = df['num_hahas'][i]
-            csv_dic['num_likes'] = df['num_likes'][i]
-            csv_dic['num_loves'] = df['num_loves'][i]
-            csv_dic['num_reactions'] = df['num_reactions'][i]
-            csv_dic['num_sads'] = df['num_sads'][i]
-            csv_dic['num_shares'] = df['num_shares'][i]
-            csv_dic['num_wows'] = df['num_wows'][i]
-            csv_dic['status_id'] = df['status_id'][i]
-            csv_dic['status_link'] = df['status_link'][i]
-            csv_dic['status_message'] = df['status_message'][i]
-            csv_dic['status_published'] = df['status_published'][i]
-            csv_dic['status_type'] = df['status_type'][i]
-            csv_dic_ls.append((csv_dic))
+        csv_dic = {}
+        csv_dic['link_name'] = df['link_name'][i]
+        csv_dic['num_angrys'] = df['num_angrys'][i]
+        csv_dic['num_comments'] = df['num_comments'][i]
+        csv_dic['num_hahas'] = df['num_hahas'][i]
+        csv_dic['num_likes'] = df['num_likes'][i]
+        csv_dic['num_loves'] = df['num_loves'][i]
+        csv_dic['num_reactions'] = df['num_reactions'][i]
+        csv_dic['num_sads'] = df['num_sads'][i]
+        csv_dic['num_shares'] = df['num_shares'][i]
+        csv_dic['num_wows'] = df['num_wows'][i]
+        csv_dic['status_id'] = df['status_id'][i]
+        csv_dic['status_link'] = df['status_link'][i]
+        csv_dic['status_message'] = df['status_message'][i]
+        csv_dic['status_published'] = df['status_published'][i]
+        csv_dic['status_type'] = df['status_type'][i]
+        csv_dic_ls.append((csv_dic))
     return csv_dic_ls
 
-#i = read_csv(tsv_path)
+status_update = read_csv(tsv_path)
 #print(i)
 
 #1b
-def get_update_most_responded_to(tsv_path, kwarg):
+def get_update_most_responded_to(status_update, kwarg):
     """
     Parameters
     ----------
@@ -46,21 +45,20 @@ def get_update_most_responded_to(tsv_path, kwarg):
     """
 
     look_for = f"num_{kwarg}"
-    df = pd.read_csv(tsv_path, sep='\t')
     highest_val = 0
     status_type = ''
     status_link = ''
     status_message = ''
 
-    for i in df.index:
-        focusd = int(df[look_for][i])
+    for i, item in enumerate(status_update):
+        focusd = int(status_update[i][look_for])
         if focusd > highest_val:
             highest_val = focusd
-            status_type = df['status_type'][i]
-            status_link = df['status_link'][i]
-            status_message = df['status_message'][i]
+            status_type = status_update[i]['status_type']
+            status_link = status_update[i]['status_link']
+            status_message = status_update[i]['status_message']
     return status_message, status_type, status_link
-#i = get_update_most_responded_to(tsv_path, 'likes')
+#i = get_update_most_responded_to(status_update, 'hahas')
 #print(i)
 
 #1c
@@ -80,5 +78,5 @@ def get_logest_update(status_updates, length_type = "tokens"):
         elif length_type == 'characters':
             i = 'i'
     return longest_update
-i = get_logest_update(tsv_path, 'tokens')
-print(i)
+#i = get_logest_update(tsv_path, 'tokens')
+#print(i)
